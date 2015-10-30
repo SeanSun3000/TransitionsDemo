8387635e3a7e9cd09745a015fb9eb1fb575d8846
jasonsun3000 <sunyongjiu2000@yahoo.com>
2015-10-30 09:52:09 +0800

    add pagetransfomer for viewPager

--- src/com/example/transitiondemo/ScreenSlideActivity.java
+++ src/com/example/transitiondemo/ScreenSlideActivity.java
@@ -8,8 +8,10 @@ import android.os.Bundle;
 import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class ScreenSlideActivity extends Activity
@@ -18,6 +20,7 @@ public class ScreenSlideActivity extends Activity
 	public static final String TAG = "hello";
 	private static final int PAGE_NUM = 5;
 	private ViewPager mViewPager;
+	private int mAnimatonModeMenuItemId = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -27,7 +30,7 @@ public class ScreenSlideActivity extends Activity
 		mViewPager = new ViewPager(this);
 		mViewPager.setId(R.id.viewPager);
 		setContentView(mViewPager);
-		
+
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 
 		ScreenSlidePagerAdapter adapter = new ScreenSlidePagerAdapter(
@@ -52,12 +55,17 @@ public class ScreenSlideActivity extends Activity
 		menu.findItem(R.id.previous).setEnabled(
 				mViewPager.getCurrentItem() > 0 ? true : false);
 
-		MenuItem next = menu.add(Menu.NONE, R.id.next, Menu.NONE, "next");
+		MenuItem next = menu
+				.add(R.id.action_flip, R.id.next, Menu.NONE, "next");
 		if (mViewPager.getCurrentItem() == PAGE_NUM - 1) {
 			next.setTitle("finish");
 		}
 		next.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM
 				| MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+		
+		if (mAnimatonModeMenuItemId != 0) {
+			menu.findItem(mAnimatonModeMenuItemId).setChecked(true);
+		}
 
 		return true;
 	}
@@ -68,15 +76,30 @@ public class ScreenSlideActivity extends Activity
 		switch (item.getItemId()) {
 		case android.R.id.home:
 			NavUtils.navigateUpTo(this, new Intent(this, MainActivity.class));
-			break;
+			return true;
 		case R.id.previous:
 			mViewPager.setCurrentItem(mViewPager.getCurrentItem() - 1);
-			break;
+			return true;
 		case R.id.next:
 			mViewPager.setCurrentItem(mViewPager.getCurrentItem() + 1);
-			break;
+			return true;
+		case R.id.action_zoom_out_page_transformer:
+			mViewPager.setPageTransformer(true, new ZoomOutPageTransformer());
+			setCheckStatus(item);
+			return true;
+		case R.id.action_depth_page_transformer:
+			mViewPager.setPageTransformer(true, new DepthPageTransformer());
+			setCheckStatus(item);
+			return true;
 		}
+
 		return super.onOptionsItemSelected(item);
+	}
+
+	private void setCheckStatus(MenuItem item)
+	{
+		mAnimatonModeMenuItemId = item.getItemId();
+		item.setChecked(true);
 	}
 
 	private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter
@@ -98,5 +121,87 @@ public class ScreenSlideActivity extends Activity
 			return PAGE_NUM;
 		}
 
+	}
+}
+
+class ZoomOutPageTransformer implements ViewPager.PageTransformer
+{
+	private static final float MIN_SCALE = 0.85f;
+	private static final float MIN_ALPHA = 0.5f;
+
+	public void transformPage(View view, float position)
+	{
+		int pageWidth = view.getWidth();
+		int pageHeight = view.getHeight();
+
+		if (position < -1) { // [-Infinity,-1)
+			// This page is way off-screen to the left.
+			view.setAlpha(0);
+
+		} else if (position <= 1) { // [-1,1]
+			// Modify the default slide transition to shrink the page as well
+			float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+			float vertMargin = pageHeight * (1 - scaleFactor) / 2;
+			float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+			final String TAG = "hello";
+			if (position < 0) {
+				view.setTranslationX(horzMargin - vertMargin / 2);
+				Log.d(TAG, "1: " + (horzMargin - vertMargin / 2));
+			} else {
+				view.setTranslationX(-horzMargin + vertMargin / 2);
+				Log.d(TAG, "2: " + (-horzMargin + vertMargin / 2));
+			}
+
+			// Scale the page down (between MIN_SCALE and 1)
+			view.setScaleX(scaleFactor);
+			view.setScaleY(scaleFactor);
+
+			// Fade the page relative to its size.
+			view.setAlpha(MIN_ALPHA + (scaleFactor - MIN_SCALE)
+					/ (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+
+		} else { // (1,+Infinity]
+			// This page is way off-screen to the right.
+			view.setAlpha(0);
+		}
+	}
+}
+
+class DepthPageTransformer implements ViewPager.PageTransformer
+{
+	private static final float MIN_SCALE = 0.75f;
+
+	public void transformPage(View view, float position)
+	{
+		int pageWidth = view.getWidth();
+
+		if (position < -1) { // [-Infinity,-1)
+			// This page is way off-screen to the left.
+			view.setAlpha(0);
+
+		} else if (position <= 0) { // [-1,0]
+			// Use the default slide transition when moving to the left page
+			view.setAlpha(1);
+			view.setTranslationX(0);
+			view.setScaleX(1);
+			view.setScaleY(1);
+
+		} else if (position <= 1) { // (0,1]
+			// Fade the page out.
+			view.setAlpha(1 - position);
+
+			// Counteract the default slide transition
+			view.setTranslationX(pageWidth * -position);
+
+			// Scale the page down (between MIN_SCALE and 1)
+			float scaleFactor = MIN_SCALE + (1 - MIN_SCALE)
+					* (1 - Math.abs(position));
+			view.setScaleX(scaleFactor);
+			view.setScaleY(scaleFactor);
+
+		} else { // (1,+Infinity]
+			// This page is way off-screen to the right.
+			view.setAlpha(0);
+		}
 	}
 }
--- src/com/example/transitiondemo/ScreenSlidePageFragment.java
+++ src/com/example/transitiondemo/ScreenSlidePageFragment.java
@@ -1,7 +1,9 @@
 package com.example.transitiondemo;
 
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -36,8 +38,30 @@ public class ScreenSlidePageFragment extends Fragment
 			Bundle savedInstanceState)
 	{
 		View v = inflater.inflate(R.layout.except_content, container, false);
+		View root = v.findViewById(R.id.content);
 		((TextView) v.findViewById(R.id.txt)).setText(getString(
 				R.string.tile_template_step, mPageNumber + 1));
+		
+		int backgoungColor = Color.BLACK;
+
+		switch (mPageNumber) {
+		case 0:
+			backgoungColor = Color.RED;
+			break;
+		case 1:
+			backgoungColor = Color.BLUE;
+			break;
+		case 2:
+			backgoungColor = Color.GREEN;
+			break;
+		case 3:
+			backgoungColor = Color.CYAN;
+			break;
+		case 4:
+			backgoungColor = Color.MAGENTA;
+			break;
+		}
+		root.setBackgroundColor(backgoungColor);
 
 		return v;
 	}
